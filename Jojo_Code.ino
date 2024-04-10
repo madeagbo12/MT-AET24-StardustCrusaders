757666ef661fe8de8613e9f75f2d4aa53352e604
madeagbo12 <165952424+madeagbo12@users.noreply.github.com>
2024-04-10 15:15:35 -0500

    Add files via upload

--- Jojo_Code.ino
+++ Jojo_Code.ino
@@ -2,20 +2,26 @@
 LED light 1 starts on
 All servos at 0 degrees
 
+//Move straizo to the appointed location
+
 If (circuit1 is completed) {
-	Servo1 rotate 180
+	Servo1 rotate 180 // turns jojo 180 degrees
 	Led light 1 = off
 	Led light 2 = on
 }
 
+// move the grenade towards straizo
+
 If (circuit2 is completed) {
-	Servo2 rotate 180
+	Servo2 rotate 180 // turns straizo 180 degrees
 	Led light 2 = off
 	Led light 3 = on
 }
 
+// pull the arrow that pulls straizo's body together
+
 If (circuit3 is completed) {
-	Servo3 rotate 180
+	Servo3 rotate 180 // flip jojo by turning him 180 degrees
 	Led light 3 = off
 }
 
