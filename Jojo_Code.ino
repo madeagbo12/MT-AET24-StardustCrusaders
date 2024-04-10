c546aa7116c599f093810be0cfcb291dadf53975
NiaNash <165952371+NiaNash@users.noreply.github.com>
2024-04-10 15:05:18 -0500

    Add files via upload
    Sudo Code

---
+++ Jojo_Code.ino
@@ -0,0 +1,31 @@
+/*
+LED light 1 starts on
+All servos at 0 degrees
+
+If (circuit1 is completed) {
+	Servo1 rotate 180
+	Led light 1 = off
+	Led light 2 = on
+}
+
+If (circuit2 is completed) {
+	Servo2 rotate 180
+	Led light 2 = off
+	Led light 3 = on
+}
+
+If (circuit3 is completed) {
+	Servo3 rotate 180
+	Led light 3 = off
+}
+
+*/
+void setup() {
+ 
+
+}
+
+void loop() {
+ 
+
+}
